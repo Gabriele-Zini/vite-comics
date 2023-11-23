8eb0326701e0b6674b4e1a73855a375e65eb9e28
Gabriele-Zini <gabrielezini845@gmail.com>
2023-11-23 22:01:39 +0100

    add: footer completed

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -19,8 +19,32 @@ export default {
                     title: "sites",
                     links: ["DC characters", "DC comics", "DC movies", "DC games", "DC videos"]
                 },
+            ],
+            socialLinks: [
+                {
+                    socialImg: "footer-facebook.png",
+                },
+                {
+                    socialImg: "footer-periscope.png",
+                },
+                {
+                    socialImg: "footer-pinterest.png",
+                },
+                {
+                    socialImg: "footer-twitter.png",
+                },
+                {
+                    socialImg: "footer-youtube.png",
+                },
             ]
         }
+    },
+    methods: {
+
+        getImage(img) {
+            return new URL(`../../public/img/${img}`, import.meta.url).href;
+        }
+
     }
 }
 </script>
@@ -38,7 +62,17 @@ export default {
                 </li>
             </ul>
         </div>
-        <div class="bottom-footer"></div>
+        <div class="bottom-footer">
+            <div>
+                <button>sign-up now!</button>
+                <div>
+                    <div>follow us</div>
+                    <ul v-for="img in socialLinks" :key="img">
+                        <li><a href=""><img :src="getImage(img.socialImg)" alt=""></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </footer>
 </template>
 
@@ -54,7 +88,7 @@ footer {
         background-image: url(../../public/img/dc-logo-bg.png);
         background-repeat: no-repeat;
         background-position: right bottom;
-        min-height: 500px;
+        min-height: 400px;
         color: white;
         padding: 0 5rem;
 
@@ -86,8 +120,10 @@ footer {
                     margin-top: .2rem;
 
                     a {
-                        color: rgba(255, 255, 255, 0.549);
+                        color: rgba(255, 255, 255, 0.389);
                         text-decoration: none;
+                        text-transform: capitalize;
+                        font-size: .9rem;
                     }
                 }
             }
@@ -96,8 +132,42 @@ footer {
     }
 
     .bottom-footer {
+        @include mixin.flex(row, space-between, center);
         min-height: 100px;
         background-color: variables.$footer-color;
+
+        div:first-child {
+            @include mixin.flex(row, space-between, center);
+            margin: 0 auto;
+            width: 70%;
+
+            button {
+                color: white;
+                background-color: variables.$footer_color;
+                border: 3px solid variables.$primary_color;
+                padding:.8rem 1.5rem;
+                text-transform: uppercase;
+                font-weight: 900;
+                font-size: 1.2rem;
+                cursor: pointer;
+            }
+
+            div:first-of-type {
+                @include mixin.flex(row, center, center);
+                gap: 1.6rem;
+
+                div {
+                    text-transform: uppercase;
+                    color: variables.$primary_color;
+                    font-size: 1.3rem;
+                    font-weight: 900;
+                }
+
+                ul {
+                    list-style: none;
+                }
+            }
+        }
     }
 }
 </style>
